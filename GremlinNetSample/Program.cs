39d7fba33dcef426607805a4320e83908b1bf050
Ravi Teja G <gnvrtptr@gmail.com>
2019-05-22 14:23:19 +0530

    Sample Commit

--- GremlinNetSample/Program.cs
+++ GremlinNetSample/Program.cs
@@ -13,6 +13,7 @@ namespace GremlinNetSample
     /// </summary>
     class Program
     {
+        // Sample Commit
         // Azure Cosmos DB Configuration variables
         // Replace the values in these variables to your own.
         private static string hostname = "your-endpoint.gremlin.cosmosdb.azure.com";
